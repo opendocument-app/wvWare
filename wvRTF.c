3d71f1679515fda77832907f8c5156c0bbf643cb
Andreas Stefl <stefl.andreas@gmail.com>
2024-09-18 15:55:44 +0200

    format security

--- wvRTF.c
+++ wvRTF.c
@@ -192,7 +192,7 @@ output_rtfUserData (rtfUserData * ud)
     rtf_output_char ('{');
 
     /* font color */
-    rtf_output (rtfColors[ud->cCol]);
+    rtf_output ("%s", rtfColors[ud->cCol]);
 
     /* font face */
     rtf_output ("\\f%d", ud->cFont);
